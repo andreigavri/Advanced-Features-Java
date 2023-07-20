c0c730de39e2bf42574340a09a3a63a93be34877
Andrei Gavriloiu <andreigavri@yahoo.com>
2023-07-20 11:59:27 +0000

    Update CannotDivedBy0Exception.java

--- src/com/sda/Exceptions/CannotDivedBy0Exception.java
+++ src/com/sda/Exceptions/CannotDivedBy0Exception.java
@@ -3,5 +3,7 @@ package com.sda.Exceptions;
 public class CannotDivedBy0Exception extends RuntimeException {
     public CannotDivedBy0Exception(String message) {
         super(message);
+
+        //New change 
     }
 }
